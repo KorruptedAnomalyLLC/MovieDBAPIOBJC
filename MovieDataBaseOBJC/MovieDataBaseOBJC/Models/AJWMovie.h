424be92ba3fd21ea61d3a42298a9a2a7919b6922
Austin West <korruptedanomaly@gmail.com>
2019-05-24 13:44:19 -0600

    ViewHierarchy set up and organized

---
+++ MovieDataBaseOBJC/MovieDataBaseOBJC/Models/AJWMovie.h
@@ -0,0 +1,17 @@
+//
+//  AJWMovie.h
+//  MovieDataBaseOBJC
+//
+//  Created by Austin West on 5/24/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AJWMovie : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MovieDataBaseOBJC/MovieDataBaseOBJC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieDataBaseOBJC
+//
+//  Created by Austin West on 5/24/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
