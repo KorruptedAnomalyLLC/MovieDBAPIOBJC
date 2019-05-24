aacce192d401a6fe777c542a725aa678d723ac20
Austin West <korruptedanomaly@gmail.com>
2019-05-24 16:45:05 -0600

    So close, but not quite finished

---
+++ MovieDataBaseOBJC/MovieDataBaseOBJC/Resources/bridgingHeader.h
@@ -0,0 +1,10 @@
+//
+//  bridgingHeader.h
+//  MovieDataBaseOBJC
+//
+//  Created by Austin West on 5/24/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import "AJWMovieController.h"
+#import "AJWMovie.h"
---
+++ MovieDataBaseOBJC/MovieDataBaseOBJC/ViewController/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MovieDataBaseOBJC
+//
+//  Created by Austin West on 5/24/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+  
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+}
